5141e6fb6f595a0bd19c9c74ee3df665a0f19840
ghostfacez <meteb7159@gmail.com>
2026-08-01 17:43:26 +0200

    Update YTDownload.ts

--- src/services/download/YTDownload.ts
+++ src/services/download/YTDownload.ts
@@ -11,14 +11,8 @@ export default class YTDownload {
     const output = `${DOWNLOAD_PATH}/${videoId}.mp3`;
     const url = `https://www.youtube.com/watch?v=${videoId}`;
 
-    // Build proxy flag only if credentials are provided
     const proxyUrl = process.env.PROXY_URL;
     const proxyFlag = proxyUrl ? `--proxy "${proxyUrl}"` : '';
-
-    // Optional: use cookies if you still want to export them later
-    const cookiesFlag = process.env.YT_COOKIES_PATH
-      ? `--cookies "${process.env.YT_COOKIES_PATH}"`
-      : '';
 
     await execAsync(
       `yt-dlp \
@@ -28,7 +22,6 @@ export default class YTDownload {
       --no-playlist \
       --no-warnings \
       ${proxyFlag} \
-      ${cookiesFlag} \
       -o "${output}" \
       "${url}"`,
     );
